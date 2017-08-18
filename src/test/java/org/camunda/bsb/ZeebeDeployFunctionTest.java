fea4f0ea5bc6e0141b81e5943d2a74714803d666
Benjamin Hoffmann <benjamin.hoffmann@camunda.com>
2017-08-18 10:01:47 +0200

    add new process model

--- src/test/java/org/camunda/bsb/ZeebeDeployFunctionTest.java
+++ src/test/java/org/camunda/bsb/ZeebeDeployFunctionTest.java
@@ -1,39 +1,32 @@
 package org.camunda.bsb;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.zeebe.client.ClientProperties;
 import io.zeebe.client.ZeebeClient;
 import io.zeebe.client.event.DeploymentEvent;
 import org.apache.log4j.Logger;
+import org.junit.Test;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
-public class ZeebeDeployFunction implements RequestHandler<ApiGatewayProxyEvent, ApiGatewayResponse> {
+public class ZeebeDeployFunctionTest {
 
-    private static final Logger LOG = Logger.getLogger(ZeebeDeployFunction.class);
+    private static final Logger LOG = Logger.getLogger(ZeebeDeployFunctionTest.class);
 
-    @Override
-    public ApiGatewayResponse handleRequest(ApiGatewayProxyEvent event, Context context) {
+
+    @Test
+    public void testDeployFunction(){
 
         try {
 
             final String BROKER_ADDRESS = "ec2-52-57-80-74.eu-central-1.compute.amazonaws.com:51015";
 
-            ObjectMapper mapper = new ObjectMapper();
-            Request request = new Request();
-
-            request = mapper.readValue(event.getBody(), Request.class);
-
-            String url = request.getUrl();
+            String url = "https://raw.githubusercontent.com/bullshit-bingo/aws-lambda-deploy-function/master/process.bpmn";
 
             LOG.debug("Getting BPMN model from " + url);
 
@@ -67,6 +60,8 @@ public class ZeebeDeployFunction implements RequestHandler<ApiGatewayProxyEvent,
 
             client.connect();
 
+            // Deploy to Zeebe
+
             LOG.debug("Deploying BPMN file to Zeebe");
 
             final DeploymentEvent deployment = client.workflows()
@@ -78,29 +73,12 @@ public class ZeebeDeployFunction implements RequestHandler<ApiGatewayProxyEvent,
             LOG.debug("Disconnecting from Zeebe broker on " + BROKER_ADDRESS);
 
             client.disconnect();
-
-            // Build the GatewayResponse
-
-            Response responseBody = new Response("Zeebe deploy from " + url + " finished successfully");
-            Map<String, String> headers = new HashMap<>();
-            headers.put("X-Powered-By", "AWS Lambda & Serverless");
-            headers.put("Content-Type", "application/json");
-
-            return ApiGatewayResponse.builder()
-                    .setStatusCode(200)
-                    .setObjectBody(responseBody)
-                    .setHeaders(headers)
-                    .build();
-
-        } catch (Exception e) {
+        }catch (Exception e ){
             e.printStackTrace();
 
-            return ApiGatewayResponse.builder()
-                    .setObjectBody(new Response(e.getMessage()))
-                    .setStatusCode(500)
-                    .build();
         }
 
     }
+
 
 }
